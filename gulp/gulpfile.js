2689a77ef22e826f98f9c94cfd4d1ff1a58baed9
George Hickman <george@ghickman.co.uk>
2015-11-07 16:10:46 +0100

    Use output directory vars everywhere

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -22,25 +22,23 @@ gulp.task('hashClean', function(cb) {
     return rimraf(distHash, cb);
 });
 gulp.task('hashCss', function() {
-    var dist = 'dist/hash';
     return gulp.src('sass/*.scss')
         .pipe(sass())  // build CSS files
         .pipe(concat('all.css'))  // combine the CSS files
         .pipe(minifyCss({'compatibility': 'ie8'}))  // minify CSS
         .pipe(hash())  // hash the filenames
-        .pipe(gulp.dest(dist))  // write the files
+        .pipe(gulp.dest(distHash))  // write the files
         .pipe(hash.manifest('assets.json', true)) // Switch to the manifest file
-        .pipe(gulp.dest(dist));  // write the manifest file
+        .pipe(gulp.dest(distHash));  // write the manifest file
 });
 gulp.task('hashJs', function() {
-    var dist = 'dist/hash';
     return gulp.src('js/**/*.js')
         .pipe(concat('all.js'))
         .pipe(uglify())
         .pipe(hash())
-        .pipe(gulp.dest(dist))
+        .pipe(gulp.dest(distHash))
         .pipe(hash.manifest('assets.json', true))
-        .pipe(gulp.dest(dist));
+        .pipe(gulp.dest(distHash));
 });
 gulp.task('hash', ['hashClean', 'hashCss', 'hashJs']);
 
@@ -52,23 +50,21 @@ gulp.task('revClean', function(cb) {
     return rimraf(distRev, cb);
 });
 gulp.task('revCss', function() {
-    var dist = 'dist/rev';
     return gulp.src('sass/*.scss')
        .pipe(sass())  // build CSS files
        .pipe(rev())  // hash the filenames
-       .pipe(gulp.dest(dist))  // write the files
+       .pipe(gulp.dest(distRev))  // write the files
        .pipe(rev.manifest()) // Switch to the manifest file
-       .pipe(gulp.dest(dist));  // write the manifest file
+       .pipe(gulp.dest(distRev));  // write the manifest file
 });
 gulp.task('revJs', function() {
-    var dist = 'dist/rev';
     return gulp.src('js/**/*.js')
         .pipe(concat('all.js'))
         .pipe(uglify())
         .pipe(rev())  // hash the filenames
-        .pipe(gulp.dest(dist))
+        .pipe(gulp.dest(distRev))
         .pipe(rev.manifest()) // Switch to the manifest file
-        .pipe(gulp.dest(dist));
+        .pipe(gulp.dest(distRev));
 });
 gulp.task('rev', ['revClean', 'revCss', 'revJs']);
 
@@ -80,7 +76,6 @@ gulp.task('revAllClean', function(cb) {
     return rimraf(distRevAll, cb);
 });
 gulp.task('revAllCss', function() {
-    var dist = 'dist/rev-all';
     var revAll = new RevAll({
         fileNameManifest: 'rev-all-manifest.json'
     });
@@ -88,12 +83,11 @@ gulp.task('revAllCss', function() {
     return gulp.src('sass/*.scss')
        .pipe(sass())  // build CSS files
        .pipe(revAll.revision()) // hash the filenames
-       .pipe(gulp.dest(dist))  // write the files
+       .pipe(gulp.dest(distRevAll))  // write the files
        .pipe(revAll.manifestFile())
-       .pipe(gulp.dest(dist));  // write the manifest file
+       .pipe(gulp.dest(distRevAll));  // write the manifest file
 });
 gulp.task('revAllJs', function() {
-    var dist = 'dist/rev-all';
     var revAll = new RevAll({
         fileNameManifest: 'rev-all-manifest.json'
     });
@@ -102,8 +96,8 @@ gulp.task('revAllJs', function() {
         .pipe(concat('all.js'))
         .pipe(uglify())
         .pipe(revAll.revision()) // hash the filenames
-        .pipe(gulp.dest(dist))
+        .pipe(gulp.dest(distRevAll))
         .pipe(revAll.manifestFile())
-        .pipe(gulp.dest(dist));
+        .pipe(gulp.dest(distRevAll));
 });
 gulp.task('revAll', ['revAllClean', 'revAllCss', 'revJs']);
