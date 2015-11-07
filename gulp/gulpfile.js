8c031ee6cf6908bf857644966c32ed5d0153ae30
George Hickman <george@ghickman.co.uk>
2015-11-07 15:54:49 +0100

    Add js task and wrap js&css tasks in top level one

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,24 +1,45 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var minifyCss = require('gulp-minify-css');
+var rimraf = require('rimraf');
 var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
 
 // manifest builders
 var hash = require('gulp-hash');
 var rev = require('gulp-rev');
 var RevAll = require("gulp-rev-all");
 
-gulp.task('hash', function() {
+gulp.task('clean', function(cb) {
+    return rimraf('./dist', cb);
+});
+
+
+/*
+ * HASH
+ */
+gulp.task('hashCss', function() {
     var dist = 'dist/hash';
     return gulp.src('sass/*.scss')
-       .pipe(sass())  // build CSS files
-       .pipe(concat('all.css'))  // combine the CSS files
-       .pipe(minifyCss({'compatibility': 'ie8'}))  // minify CSS
-       .pipe(hash())  // hash the filenames
-       .pipe(gulp.dest(dist))  // write the files
-       .pipe(hash.manifest('assets.json')) // Switch to the manifest file
-       .pipe(gulp.dest(dist));  // write the manifest file
+        .pipe(sass())  // build CSS files
+        .pipe(concat('all.css'))  // combine the CSS files
+        .pipe(minifyCss({'compatibility': 'ie8'}))  // minify CSS
+        .pipe(hash())  // hash the filenames
+        .pipe(gulp.dest(dist))  // write the files
+        .pipe(hash.manifest('assets.json', true)) // Switch to the manifest file
+        .pipe(gulp.dest(dist));  // write the manifest file
 });
+gulp.task('hashJs', function() {
+    var dist = 'dist/hash';
+    return gulp.src('js/**/*.js')
+        .pipe(concat('all.js'))
+        .pipe(uglify())
+        .pipe(hash())
+        .pipe(gulp.dest(dist))
+        .pipe(hash.manifest('assets.json', true))
+        .pipe(gulp.dest(dist));
+});
+gulp.task('hash', ['clean', 'hashCss', 'hashJs']);
 
 
 gulp.task('rev', function() {
